246202ee06e2f79e48bc8e9f5c013ef91c5a9d57
AC <aleksandar.cincarevic@lexutb.se>
2021-09-30 16:05:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ConsoleApp_EntityFramework_CF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console App - EntityFramework/CF");
+            Console.WriteLine("\n\n- Press enter to add a <Car> to DB");
+
+            try
+            {
+                VehicleContext db = new VehicleContext();
+                Car newCar = new("Saab", "95 Turbo", 2002, 210);
+                db.Cars.Add(newCar);
+                db.SaveChanges();
+                Console.WriteLine("<Car> was added to DB");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Failed to save <Car> to DB\nErr: {ex}");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }   
+        }
+    }
+
+    class Car
+    {
+        public Car(string brand, string model, int year, int speed)
+        {
+            Brand = brand;
+            Model = model;
+            Year = year;
+            Speed = speed;
+        }
+
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Speed { get; set; }
+    }
+
+    class VehicleContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Vehicle_EF_CF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
